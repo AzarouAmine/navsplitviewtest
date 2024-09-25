581b76734133a2696dcbeb9383759f2a566e80cc
AZAROU Mohammed-Amine <maazarou@sqli.com>
2024-09-25 09:30:53 +0200

    Initial Commit

---
+++ navsplitviewtest/navsplitviewtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  navsplitviewtestApp.swift
+//  navsplitviewtest
+//
+//  Created by Mohammed Amine AZAROU on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct navsplitviewtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
